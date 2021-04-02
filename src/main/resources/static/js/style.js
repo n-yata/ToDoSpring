74e2777a07f80f1f026bd06ca20b192ecc3b4b09
n-yata <yata1214@gmail.com>
2021-04-02 15:53:40 +0900

    スタイルの追加・更新

---
+++ src/main/resources/static/js/style.js
@@ -0,0 +1,13 @@
+/**
+ *
+ */
+ $(function(){
+    $('#loginBtn').on('mouseover', function(){
+        $('#loginBtn').animate({
+            opacity: 0,
+            fontSize: '0px'
+        },
+        1500
+        );
+    });
+});
